9275f8189f4a2b62d48f964770fabb3eb55612a9
Bhuvanes0987 <bhuvanes0987@gmail.com>
2026-07-22 00:23:15 +0530

    backend chnages

--- VBN-Best-boost-performance/frontend/src/app/Environment/Environment.ts
+++ VBN-Best-boost-performance/frontend/src/app/Environment/Environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8900',
+  // apiUrl: 'http://127.0.0.1:8900',
   //production
-  //  apiUrl: '/api',
+  apiUrl: '/api',
   razorpayKeyId: 'rzp_test_XXXXXXXXXXXXXXXX',
 };
